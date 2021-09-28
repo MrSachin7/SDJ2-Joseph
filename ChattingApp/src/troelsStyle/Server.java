b0e4f4f4bbc657ea99ad415612c084d37e68721d
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2021-09-28 22:51:35 +0200

    chatting app

--- ChattingApp/src/troelsStyle/Server.java
+++ ChattingApp/src/troelsStyle/Server.java
@@ -5,10 +5,12 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         final int PORT = 2810;
+        ArrayList<String> log = new ArrayList<>();
         System.out.println("Server started.........");
         ServerSocket serverSocket = new ServerSocket(PORT);
         Socket socket = serverSocket.accept();
@@ -22,9 +24,11 @@ public class Server {
         if (msg.equals("connect")) {
             System.out.println(client + " has now been given access to the server");
             outTOClient.writeObject("Server connected");
-            while (!msg.toUpperCase().equals("EXIT")) {
-                System.out.println(client+" sent "+msg);
+            while (!msg.toUpperCase().equals("EXIT")) {;
+
                 msg =(String) inFromClient.readObject();
+                log.add(msg);
+                System.out.println(log.get(log.size()-1));
             }
         }
         else
